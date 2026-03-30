e3ade447c57fe66e1abaf316db473ce6a507dda5
dgKim1 <kimdg3556@naver.com>
2026-03-30 22:00:28 +0900

    feat: "메모 프로젝트 JPA적용"

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Component
@@ -32,7 +33,7 @@ public class MemoService {
 
   public List<MemoResponseDto> getMemos() {
 
-    return memoRepository.findAll();
+    return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
 
 
   }
@@ -47,34 +48,32 @@ public class MemoService {
     return memoResponseDto;
   }
 
+  @Transactional
   public Long updateMemo(Long id, MemoRequestDto requestDto) {
     // 해당 메모가 DB에 존재하는지 확인
-    Memo memo = memoRepository.findById(id);
-    if (memo != null) {
-      memoRepository.update(id,requestDto);
-
-
-      return id;
-    } else {
-      throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-    }
+    Memo memo = findMemo(id);
+    memo.update(requestDto);
+    return id;
 
   }
 
 
   public Long deleteMemo(Long id) {
     // 해당 메모가 DB에 존재하는지 확인
-    Memo memo = memoRepository.findById(id);
-    ;
-    if (memo != null) {
-      memoRepository.delete(id);
+    Memo memo = findMemo(id);
 
-
-      return id;
-    } else {
-      throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-    }
+    //메모 삭제
+    memoRepository.delete(memo);
+    return id;
   }
+
+
+  private Memo findMemo(Long id) {
+    return memoRepository.findById(id).orElseThrow(()->
+            new IllegalArgumentException("선택한 메모는 존재하지 않습니다.")
+        );
+  }
+
 
 
 
